77c87354c27e78774e249f957724a9d4da2a2f58
David Rodriguez Miranda <wolfs_pride@outlook.es>
2022-08-16 19:49:01 +0200

    alert

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -6,10 +7,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alert.page.scss'],
 })
 export class AlertPage implements OnInit {
+  constructor(private alertController: AlertController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async onClick(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('Click en ok');
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'rojo',
+          handler: () => {},
+        },
+      ],
+      backdropDismiss: false,
+    });
+
+    await alert.present();
   }
 
+  async presentAlertInputs(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Please enter your info',
+      buttons: ['OK'],
+      inputs: [
+        {
+          placeholder: 'Name',
+        },
+        {
+          placeholder: 'Nickname (max 8 characters)',
+          attributes: {
+            maxlength: 8,
+          },
+        },
+        {
+          type: 'number',
+          placeholder: 'Age',
+          min: 1,
+          max: 100,
+        },
+        {
+          type: 'textarea',
+          placeholder: 'A little about yourself',
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
