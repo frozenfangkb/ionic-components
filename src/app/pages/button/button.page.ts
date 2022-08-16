33ee50ade23a281fcdde19d404bcb487e03f3061
David Rodriguez Miranda <wolfs_pride@outlook.es>
2022-08-16 20:20:56 +0200

    Buttons and corrections

---
+++ src/app/pages/button/button.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.page.html',
+  styleUrls: ['./button.page.scss'],
+})
+export class ButtonPage implements OnInit {
+  favourite = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onClick() {
+    this.favourite = !this.favourite;
+  }
+}
